d8c8ed0ae9b87fe486997ce785206d1b2a00a64a
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2018-04-30 19:08:06 +0530

    Constructor Method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Factory Method
 function createCirle(radius){
  return{
   radius,
@@ -6,6 +7,20 @@ function createCirle(radius){
   }
  };
 }
-
 const  circle = createCirle(1);
 circle.draw();
+
+// Construction Function
+function Circle(radius){
+  this.radius = radius;
+  this.draw = function(){
+    console.log('draw')
+  }
+}
+const another = new Circle(1);
+// New keywords create new refereance and assign the variable to it
+// if we dont add new keyword it would reference to window object which is a global object which could create a bug in application
+// First it creates an empty object
+// Second it set this to point to this object.
+// Third it would return. When using new it would automatically return new object. We dont need to add return statement
+// if we use this and new method we it refers as constructor method.
